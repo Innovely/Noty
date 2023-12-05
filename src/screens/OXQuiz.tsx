8b6937c448f30d47ae3690159702640bb788a40c
Yeongchan <gy2553@naver.com>
2023-12-05 23:36:12 +0900

    OXQuizaddModal

--- src/screens/OXQuiz.tsx
+++ src/screens/OXQuiz.tsx
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Modal,
+} from "react-native";
 
 const OXQuiz = () => {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [modalVisibleCorrect, setModalVisibleCorrect] = useState(false);
+  const [modalVisibleIncorrect, setModalVisibleIncorrect] = useState(false);
+
+  const handleAnswer = (userAnswer) => {
+    // 여기서 실제 정답과 사용자의 선택을 비교하여 모달을 보여줄 수 있습니다.
+    const isCorrect = userAnswer === "O"; // 예시로 O를 정답으로 가정합니다.
+
+    if (isCorrect) {
+      setModalVisibleCorrect(true);
+    } else {
+      setModalVisibleIncorrect(true);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -19,18 +39,68 @@ const OXQuiz = () => {
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.buttonY}
-            onPress={() => setSelectedOption("O")}
+            onPress={() => {
+              setSelectedOption("O");
+              handleAnswer("O");
+            }}
           >
             <Text style={styles.buttonText}>{"O"}</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.buttonN}
-            onPress={() => setSelectedOption("X")}
+            onPress={() => {
+              setSelectedOption("X");
+              handleAnswer("X");
+            }}
           >
             <Text style={styles.buttonText}>{"X"}</Text>
           </TouchableOpacity>
         </View>
       </View>
+
+      {/* 정답 모달 */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisibleCorrect}
+        onRequestClose={() => {
+          setModalVisibleCorrect(!modalVisibleCorrect);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>정답입니다!</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => setModalVisibleCorrect(!modalVisibleCorrect)}
+            >
+              <Text style={styles.modalButtonText}>닫기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      {/* 오답 모달 */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisibleIncorrect}
+        onRequestClose={() => {
+          setModalVisibleIncorrect(!modalVisibleIncorrect);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>오답입니다!</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => setModalVisibleIncorrect(!modalVisibleIncorrect)}
+            >
+              <Text style={styles.modalButtonText}>닫기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -71,7 +141,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#000000",
   },
-
   buttonY: {
     marginTop: 0,
     marginLeft: 35,
@@ -90,7 +159,6 @@ const styles = StyleSheet.create({
     width: 130,
     height: 40,
   },
-
   buttonText: {
     fontSize: 16,
     color: "#ffffff",
@@ -98,7 +166,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginLeft: 10,
   },
-
   quizImage: {
     width: 90,
     height: 90,
@@ -117,6 +184,38 @@ const styles = StyleSheet.create({
   },
   headContainer: {
     flexDirection: "row",
+  },
+  // 모달 스타일
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    padding: 20,
+    width: 375,
+    height: 385,
+    alignItems: "center",
+    marginBottom: 100,
+  },
+  modalText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: "#00A184",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 250, // 사진 넣을것 추가
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
